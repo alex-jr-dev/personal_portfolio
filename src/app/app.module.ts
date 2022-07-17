cd598d1c672626729e8c17770cf57ba1f2288f81
Alessandro <geremia.alessandro@outlook.com>
2022-07-18 01:16:12 +0200

    Added styles folder, changed font, completed desktop top-bar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,23 @@
 import { NgModule } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
 import { BrowserModule } from '@angular/platform-browser';
-
-import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { TopBarComponent } from './components/top-bar/top-bar.component';
+import { AppComponent } from './app.component';
 import { FullPageComponent } from './components/full-page/full-page.component';
+import { TopBarComponent } from './components/top-bar/top-bar.component';
+import { TopBarLinksComponent } from './components/top-bar/top-bar-links/top-bar-links.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     TopBarComponent,
-    FullPageComponent
+    FullPageComponent,
+    TopBarLinksComponent,
   ],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatButtonModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/top-bar/top-bar.component.ts
+++ src/app/components/top-bar/top-bar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { CONTACT_LABEL, LINKS, NAME } from 'src/app/constants';
 
 @Component({
   selector: 'app-top-bar',
@@ -6,6 +7,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./top-bar.component.scss']
 })
 export class TopBarComponent implements OnInit {
+  name = NAME;
+  contactMe = CONTACT_LABEL;
+  links = LINKS;
 
   constructor() { }
 
