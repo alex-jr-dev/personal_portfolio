a3c138d4596774ef83fe2627ac792ea16b1a1300
Alessandro <geremia.alessandro@outlook.com>
2022-07-18 00:36:23 +0200

    Added full-page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { TopBarComponent } from './components/top-bar/top-bar.component';
+import { FullPageComponent } from './components/full-page/full-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TopBarComponent,
+    FullPageComponent
   ],
   imports: [
     BrowserModule,
